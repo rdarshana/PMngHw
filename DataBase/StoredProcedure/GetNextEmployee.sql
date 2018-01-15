62e141947e54175aad8403d4e8c12d0aa53d0e8e
rdarshana <rukshan.asela87@gmail.com>
2018-01-15 13:31:32 +0530

    Get next employeeId added

--- DataBase/StoredProcedure/GetNextEmployee.sql
+++ DataBase/StoredProcedure/GetNextEmployee.sql
@@ -5,18 +5,7 @@ BEGIN
 DECLARE @RowCount INTEGER;
 DECLARE @SelectQuery VARCHAR(200);
 
-	--SET @SelectQuery = 'SELECT COUNT(*) FROM dbo.'+ @EmployeeType +  ' WHERE YEAR(CreationDate) = YEAR( getdate())';
+	SET @SelectQuery = 'SELECT COUNT(*) FROM dbo.'+ @EmployeeType +  ' WHERE YEAR(CreationDate) = YEAR( getdate())';
+	exec (@SelectQuery);
 
-	SET @SelectQuery = 'SELECT COUNT(*) FROM dbo.@EmployeeType WHERE YEAR(CreationDate) = YEAR( getdate())';
-
-	EXEC dbo.sp_executesql @SelectQuery,N'@EmployeeType varchar(15)', N'@RowCount INTEGER OUTPUT',@EmployeeType, @RowCount OUTPUT;
-
-	--exec @RowCount = @SelectQuery;
-
-	--PRINT @SelectQuery;
-	--PRINT @RowCount
-	--@RowCount = 
-
-	--SELECT CONCAT('PAT', @RowCount +1 ,'-', YEAR(getdate()));
-	--SELECT @NextPatientId =  CONCAT(@RowCount +1 ,'-', YEAR(getdate()));
 END
