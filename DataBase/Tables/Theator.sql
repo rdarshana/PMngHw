d5af755030be6ef8503ec0e9fd96b4348d446446
rdarshana <rukshan.asela87@gmail.com>
2018-02-26 06:56:16 +0530

    Updated length updated

--- DataBase/Tables/Theator.sql
+++ DataBase/Tables/Theator.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Theator] (
     [TheatorId]       VARCHAR (5)  NOT NULL,
     [Description]     VARCHAR (200)  NULL,
-    [IsActive]        VARCHAR (5)  NOT NULL,
+    [IsActive]        VARCHAR (8)  NOT NULL,
     PRIMARY KEY CLUSTERED (TheatorId)
 );
 
